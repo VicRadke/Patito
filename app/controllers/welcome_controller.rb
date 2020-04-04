f4ccc7b6b4bc44b4664ab14f82a499a5aa9c42a4
Victor M. Mendoza <willradke1@gmail.com>
2020-04-03 20:16:23 -0600

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,18 @@ class WelcomeController < ApplicationController
 
     def index
         @a = "prueba"
-    end    
+        @users = User.all 
+        puts "probando"
+        @users.each do |user|
+            puts user.name
+        end 
+        puts "fin probando"
+    end 
+    def update
+        @user =User.find(2)
+        puts @user
+       
+        puts @user.age 
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#User.create(name: "Pedrito", age: 19, cellphone: "525521382914")
+
+User.create(name: "JuanDos",age: 21, cellphone: "525521382914")
